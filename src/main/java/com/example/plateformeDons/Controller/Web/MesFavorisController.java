4a1ee89275568bf3876484634ea1f71776aa72e0
ayoubhatmi <ayoub.hatmi18@gmail.com>
2024-12-15 18:46:05 +0100

    Minor fix

--- src/main/java/com/example/plateformeDons/Controller/Web/MesFavorisController.java
+++ src/main/java/com/example/plateformeDons/Controller/Web/MesFavorisController.java
@@ -1,6 +1,9 @@
 package com.example.plateformeDons.Controller.Web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +24,7 @@ public class MesFavorisController {
 
     @Autowired
     public MesFavorisController(RestTemplate restTemplate) {
-    this.restTemplate = restTemplate;
+        this.restTemplate = restTemplate;
     }
 
     /**
@@ -33,19 +36,18 @@ public class MesFavorisController {
     @GetMapping("/favoris")
     public String home(Model model, HttpServletRequest request) {
         // Fetch ads from the API
-        List<Annonce> annonces = fetchAdsFromApi();
+        List<Annonce> annonces = fetchAdsFromApi(request);
 
-
-            boolean isLoggedIn = false;
-            Cookie[] cookies = request.getCookies();
-    if (cookies != null) {
-        for (Cookie cookie : cookies) {
-            if ("jwtToken".equals(cookie.getName()) && cookie.getValue() != null) {
-                isLoggedIn = true;
-                break;
+        boolean isLoggedIn = false;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwtToken".equals(cookie.getName()) && cookie.getValue() != null) {
+                    isLoggedIn = true;
+                    break;
+                }
             }
         }
-    }
         model.addAttribute("isLoggedIn", isLoggedIn); 
         // Add ads to the model
         model.addAttribute("annonces", annonces);
@@ -55,13 +57,37 @@ public class MesFavorisController {
     }
 
     /**
-     * Fetch ads from the API.
+     * Fetch ads from the API with Bearer token in the header.
      *
+     * @param request The HTTP request to get the JWT token.
      * @return List of ads.
      */
-    private List<Annonce> fetchAdsFromApi() {
-        String apiUrl = "http://localhost:8080/api/annonces";
-        Annonce[] adsArray = restTemplate.getForObject(apiUrl, Annonce[].class);
-        return Arrays.asList(adsArray);
+    private List<Annonce> fetchAdsFromApi(HttpServletRequest request) {
+        String apiUrl = "http://localhost:8080/api/favoris/favoris";
+        
+        // Get the JWT token from cookies
+        String jwtToken = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwtToken".equals(cookie.getName())) {
+                    jwtToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        
+        // Set the Bearer token in the request header
+        HttpHeaders headers = new HttpHeaders();
+        if (jwtToken != null) {
+            headers.set("Authorization", "Bearer " + jwtToken);
+        }
+
+        // Create a request entity with the headers
+        ResponseEntity<Annonce[]> response = restTemplate.exchange(
+            apiUrl, HttpMethod.GET, new org.springframework.http.HttpEntity<>(headers), Annonce[].class);
+
+        // Return the list of ads
+        return Arrays.asList(response.getBody());
     }
 }
